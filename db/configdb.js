68b6bb32d166236a6046652d74dce57cebc4065e
Elie <eliegebransoued@gmail.com>
2021-02-02 17:25:28 +0100

    We installed dotenv,

---
+++ db/configdb.js
@@ -0,0 +1,19 @@
+import { Pool } from "pg";
+
+const { DBUSER, DBHOST, DBNAME, DBPASS, DBPORT } = process.env;
+
+const pool = new Pool({
+  user: DBUSER,
+  host: DBHOST,
+  database: DBNAME,
+  password: DBPASS,
+  port: DBPORT,
+});
+
+const db = {
+  query: (text, params) => {
+    return pool.query(text, params);
+  },
+};
+
+export default db;
