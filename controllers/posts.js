127e6c93392ea5e409f380c6107d234f6e9ee2e1
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-03 17:36:41 +0100

    experementing on postsController

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,21 +3,29 @@ import Queries from "../db/queries";
 
 const postsController = {
   getPosts: async (req, res) => {
+    const { sort, search, topic } = req.query;
+    let query;
+    if (sort) {
+      query = [Queries.sortedByRating, sort];
+    } else if (search) {
+      query = [Queries.sortedByRating, search];
+    } else {
+      query = [Queries.getAll];
+    }
+    console.log(query);
     try {
-      console.log(Queries.getAll);
-      const result = await db.query(Queries.getAll);
+      const result = await db.query(query[0], query[1]);
       res.json({
         message: "sucessfully sent",
         status: 200,
         data: result.rows,
       });
     } catch (e) {
-      error(Error(e));
+      console.error(Error(e));
     }
   },
   getById: async (req, res) => {
     try {
-      console.log(Queries.getAll);
       const result = await db.query(Queries.getById, [req.params.id]);
       res.json({
         message: "sucessfully sent",
@@ -25,7 +33,7 @@ const postsController = {
         data: result.rows,
       });
     } catch (e) {
-      error(Error(e));
+      console.error(Error(e));
     }
   },
 };
