a300d731533aedec4324e4288a6733973016885a
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-04 16:37:31 +0100

    add query string to  search posts for title and description

--- db/queries.js
+++ db/queries.js
@@ -122,6 +122,37 @@ const queryPosts = {
     JOIN users AS u ON u.id = p.user_id 
     JOIN premium AS pr ON pr.id = u.premium_id
     WHERE (t.name) LIKE $1`,
+  searchPosts: 
+    `SELECT 
+      json_build_object(
+        'id', p.id,
+        'title', p.title,
+        'description', p.description,
+        'rating', p.rating,
+        'image_url', p.image_url,
+        'topic_id', p.topic_id,
+        'topic', json_build_object(
+          'id', t.id,
+          'name', t.name
+        ),
+        'user_id', p.user_id,
+        'user', json_build_object(
+          'id', u.id,
+          'username', u.username,
+          'email', u.email,
+          'avatar', u.avatar_image_url,
+          'premium_id', u.premium_id,
+          'premium', json_build_object(
+            'id', pr.id,
+            'level', pr.level
+          )
+        )
+      )  
+    FROM posts AS p
+    JOIN topics AS t ON t.id = p.topic_id
+    JOIN users AS u ON u.id = p.user_id 
+    JOIN premium AS pr ON pr.id = u.premium_id
+    WHERE p.title LIKE $1 OR p.desription LIKE $1`,
 };
 
 export default queryPosts;
