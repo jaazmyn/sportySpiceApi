cd6c97d6cb3f9be6d7c6ea344f51417fe58494cc
Jasmin Jenull <jasminjenull@gmail.com>
2021-02-04 16:12:40 +0100

    Import regenerator-runtime

--- tests/posts.spec.js
+++ tests/posts.spec.js
@@ -1,38 +1,38 @@
-// // Import the dependencies for testing
-// import chai from "chai";
-// import chaiHttp from "chai-http";
-// import regeneratorRuntime from "regenerator-runtime";
-// import app from "../index";
+// Import the dependencies for testing
+import chai from "chai";
+import chaiHttp from "chai-http";
+import regeneratorRuntime from "regenerator-runtime";
+import app from "../index";
 
-// // Configure chai
-// chai.use(chaiHttp);
-// chai.should();
-// describe("Posts", () => {
-//   describe("GET /posts", () => {
-//     // Test to get all posts records
-//     it("should get all post records", (done) => {
-//       chai
-//         .request(app)
-//         .get("/posts")
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.be.a("object");
-//           done();
-//         });
-//     });
-//     // Test to get single post record
-//     it("should get a single post record by id", (done) => {
-//       const id = 1;
-//       chai
-//         .request(app)
-//         .get(`/posts/${id}`)
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.be.a("object");
-//           done();
-//         });
-//     });
-//   });
-// });
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+describe("Posts", () => {
+  describe("GET /posts", () => {
+    // Test to get all posts records
+    it("should get all post records", (done) => {
+      chai
+        .request(app)
+        .get("/posts")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+    // Test to get single post record
+    it("should get a single post record by id", (done) => {
+      const id = 1;
+      chai
+        .request(app)
+        .get(`/posts/${id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+});
 
 // testy test
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const regeneratorRuntime = require("regenerator-runtime");
 
 module.exports = {
   mode: "production",
