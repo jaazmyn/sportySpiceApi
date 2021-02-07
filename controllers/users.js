ee7c27b60d9c63c87451347bd356ef76ce8b82db
Jasmin Jenull <jasminjenull@gmail.com>
2021-02-07 20:27:46 +0100

    Fix error in console

--- controllers/users.js
+++ controllers/users.js
@@ -19,14 +19,15 @@ const usersController = {
     try {
       const result = await db.query(Queries.getById, [req.params.id]);
 
-      if (result.rows.length === 0)
+      if (result.rows.length === 0) {
         res.json({ message: "No entry", status: 404 });
-
-      res.json({
-        message: "Sucessfully sent",
-        status: res.statusCode,
-        data: result.rows,
-      });
+      } else {
+        res.json({
+          message: "Sucessfully sent",
+          status: res.statusCode,
+          data: result.rows,
+        });
+      }
     } catch (e) {
       console.error(Error(e));
     }
