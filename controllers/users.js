15dd01c1ad7b391059593724b18264c3714ed637
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-07 12:57:24 +0100

    implement users controller for get all users and get user by id

---
+++ controllers/users.js
@@ -0,0 +1,33 @@
+import db from "../db/config.db";
+import Queries from "../db/usersQueries";
+
+const usersController = {
+  getUsers: async (req, res) => {
+    try {
+      console.log(Queries.getAll);
+      const result = await db.query(Queries.getAll);
+      res.json({
+        message: "sucessfully sent",
+        status: res.statusCode,
+        count: result.rows.length,
+        data: result.rows,
+      });
+    } catch (e) {
+      console.error(Error(e));
+    }
+  },
+  getUsersById: async (req, res) => {
+    try {
+      const result = await db.query(Queries.getById, [req.params.id]);
+      res.json({
+        message: "sucessfully sent",
+        status: res.statusCode,
+        data: result.rows,
+      });
+    } catch (e) {
+      console.error(Error(e));
+    }
+  },
+};
+
+export default usersController;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import usersController from "../controllers/users";
 
 const router = express.Router();
 
-router.get("/:id", usersController.getById);
+router.get("/:id", usersController.getUsersById);
 router.get("/", usersController.getUsers);
 
 export default router;
