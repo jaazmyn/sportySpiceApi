746dcff8755408176ef266394a05177caf51cf16
Jasmin Jenull <jasminjenull@gmail.com>
2021-02-07 20:13:52 +0100

    Send 404 if id doesn't exist

--- controllers/users.js
+++ controllers/users.js
@@ -18,8 +18,12 @@ const usersController = {
   getUsersById: async (req, res) => {
     try {
       const result = await db.query(Queries.getById, [req.params.id]);
+
+      if (result.rows.length === 0)
+        res.json({ message: "No entry", status: 404 });
+
       res.json({
-        message: "sucessfully sent",
+        message: "Sucessfully sent",
         status: res.statusCode,
         data: result.rows,
       });
